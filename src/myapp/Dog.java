91695cd812c003db60eec1ea98ba7c3a632163f5
atitaya03 <Priow111555@gmail.com>
2023-07-03 12:01:05 +0700

    Update Cat and add Dog

--- src/myapp/Dog.java
+++ src/myapp/Dog.java
@@ -1,15 +1,13 @@
 package myapp;
 
-public class Cat {
+public class Dog {
     private String name;
     private int age;
-    Cat(String name,int age){
+
+    public Dog(String name, int age) {
         this.name = name;
         this.age = age;
+
     }
 
-
-    public String getName() {
-        return name;
-    }
 }
